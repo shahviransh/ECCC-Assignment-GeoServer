b3930430467893a433cad6a49b043a469dffc3a8
Viransh Shah <shahv47@mcmaster.ca>
2024-07-28 17:06:31 -0400

    Comments added

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,17 @@
 <template>
-  <div id="map" class="h-screen w-full"></div>
+  <div id="app">
+    <!-- Use the LeafletMap component -->
+    <LeafletMap />
+  </div>
 </template>
 
 <script>
-import { onMounted, onBeforeUnmount, ref } from 'vue';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import './Tween.js';
+import LeafletMap from './components/LeafletMap.vue'; // Import the LeafletMap component
 
 export default {
-  name: 'LeafletMap',
-  setup() {
-    const map = ref(null);
-
-    onMounted(() => {
-      map.value = L.map('map').setView([25.5, 118.88], 12);
-
-      // Add the base tile layer
-      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map.value);
-
-      // Add GeoServer WMS layer with the provided link
-      L.tileLayer.wms('http://localhost:9090/geoserver/ECCCGeoServer/wms', {
-        layers: 'ECCCGeoServer:basin',
-        format: 'image/png',
-        transparent: true,
-        version: '1.1.0',
-        request: 'GetMap',
-        srs: 'EPSG:4326',
-        styles: '',
-      }).addTo(map.value);
-    });
-
-    onBeforeUnmount(() => {
-      if (map.value) {
-        map.value.remove();
-      }
-    });
-
-    return {
-      map,
-    };
+  name: 'App',
+  components: {
+    LeafletMap, // Register the LeafletMap component
   },
 };
 </script>
-
-<style scoped>
-#map {
-  width: 99vw;
-  height: 97vh;
-}
-</style>
---
+++ src/components/LeafletMap.vue
@@ -0,0 +1,57 @@
+<template>
+    <!-- This div will hold the Leaflet map -->
+    <div id="map" class="h-screen w-full"></div>
+</template>
+
+<script>
+import { onMounted, onBeforeUnmount, ref } from 'vue';
+import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
+import L from 'leaflet'; // Import Leaflet library
+
+export default {
+    name: 'LeafletMap',
+    setup() {
+        const map = ref(null); // Create a reactive reference for the map
+
+        onMounted(() => {
+            // Initialize the map when the component is mounted
+            map.value = L.map('map').setView([25.5, 118.88], 12);
+
+            // Add the base tile layer from OpenStreetMap
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+            }).addTo(map.value);
+
+            // Add GeoServer WMS layer with the provided link
+            L.tileLayer.wms('http://localhost:9090/geoserver/ECCCGeoServer/wms', {
+                layers: 'ECCCGeoServer:basin',
+                format: 'image/png',
+                transparent: true,
+                version: '1.1.0',
+                request: 'GetMap',
+                srs: 'EPSG:4326',
+                styles: '',
+            }).addTo(map.value);
+        });
+
+        onBeforeUnmount(() => {
+            // Clean up the map instance when the component is unmounted
+            if (map.value) {
+                map.value.remove();
+            }
+        });
+
+        return {
+            map,
+        };
+    },
+};
+</script>
+
+<style scoped>
+/* Style for the map container */
+#map {
+    width: 99vw;
+    height: 97vh;
+}
+</style>
